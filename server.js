bc6e0a1e8e9bbd272a56d4e2ca165be6ee24fc1e
naranjito72 <theclick36@gmail.com>
2021-10-25 11:58:34 +0200

    starting conection DDBB

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ import app from "./app.js";
 // const PORT = 3000;
 
 // app.listen(PORT,()=>console.log(`listening on ${PORT}`));
+console.log(`listening on ${process.env.PORT}`)
 app.listen(process.env.PORT,()=>console.log(`listening on ${process.env.PORT}`));
